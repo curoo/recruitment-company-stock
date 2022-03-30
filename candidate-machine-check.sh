1950aa41054f86be776caddfaee50e989c51ca00
expelledboy Anthony Jackson <102334+expelledboy@users.noreply.github.com>
2022-03-30 17:45:09 +0200

    chore: add pre-meeting system check script

---
+++ candidate-machine-check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function bin_check() {
+  which $1 > /dev/null; exists=$?
+  [ $exists -ne 0 ] && echo -e "\nERROR: $1 not found" && exit 1
+
+  printf "%-20s" $1
+  [ $exists -eq 0 ] && echo "OK" || echo "MISSING"
+}
+
+echo "Checking for required binaries..."
+echo
+
+bin_check "python3"
+bin_check "npm"
+bin_check "docker"
+bin_check "docker-compose"
+bin_check "git"
+bin_check "make"
+
+echo
+echo "System check complete."
