7f88d0b401f265e44ce06c30373f79c196a091cd
expelledboy 102334+expelledboy@users.noreply.github.com <102334+expelledboy@users.noreply.github.com>
2022-08-09 01:45:19 +0200

    Reduce golden data

--- packages/api/src/database.ts
+++ packages/api/src/database.ts
@@ -15,21 +15,9 @@ export const database: { users: User[]; companies: Company[] } = {
   users: [
     {
       id: "3dcb4a1f-0c91-42c5-834f-26d227c532e2",
-      email: "jb@jamesbe.com",
+      email: "jd@blad.com",
       passcode: "1234",
-      name: "James",
-    },
-    {
-      id: "ea120573-2eb4-495e-be48-1b2debac2640",
-      email: "alex@example.com",
-      passcode: "9876",
-      name: "Alex",
-    },
-    {
-      id: "2ee1c07c-7537-48f5-b5d8-8740e165cd62",
-      email: "sachin@example.com",
-      passcode: "1234",
-      name: "Sachin",
+      name: "John Doe",
     },
   ],
   companies: [
