95893a4447bd6e613faf28ffa87875abd0899a7c
expelledboy 102334+expelledboy@users.noreply.github.com <102334+expelledboy@users.noreply.github.com>
2022-08-09 03:03:19 +0200

    Fix breaking unit test

--- packages/api/src/database.ts
+++ packages/api/src/database.ts
@@ -15,7 +15,7 @@ export const database: { users: User[]; companies: Company[] } = {
   users: [
     {
       id: "3dcb4a1f-0c91-42c5-834f-26d227c532e2",
-      email: "jd@blad.com",
+      email: "jd@example.com",
       passcode: "1234",
       name: "John Doe",
     },
